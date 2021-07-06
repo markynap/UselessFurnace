3a9f0c22648710b35ce7e62aab8734c335aa2477
Mark Napolitano <mark.napolitano@softwaresolutions802.com>
2021-07-06 00:37:43 -0400

    Added Tracker for BNB Spent on Buy/Burn of USELESS

--- UselessFurnace.sol
+++ UselessFurnace.sol
@@ -600,10 +600,12 @@ contract UselessFurnace is Context, Ownable {
   address payable private _uselessAddr = payable(0x2cd2664Ce5639e46c6a3125257361e01d0213657);
  
   // burn wallet address
-  address payable private _burnWallet = payable(0x000000000000000000000000000000000000dead);
+  address payable private _burnWallet = payable(0x0000000000000000000000000000000000000001);
   
   // what percent of our BNB we buy and burn USELESS with
   uint8 public _buyAndBurnPercent = 100;
+  
+  uint256 public _totalBNBUsedToBuyAndBurnUSELESS = 0;
   
   // Initialize Pancakeswap Router
   IUniswapV2Router02 private uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E);
@@ -643,6 +645,8 @@ contract UselessFurnace is Context, Ownable {
      uint256 buyBurnBalance = calculateBuyBurnAmount(balance);
      
      buyAndBurnUseless(buyBurnBalance);
+     
+     _totalBNBUsedToBuyAndBurnUSELESS = _totalBNBUsedToBuyAndBurnUSELESS.add(buyBurnBalance);
      
      emit BuyAndBurn(buyBurnBalance);
   }
